dfb62c35d33074eb62ea1f75147494230f85b738
Manshoor Ali <manshoor110@gmail.com>
2025-01-23 07:18:40 +0500

    swagger css fix for vercel

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -9,5 +9,17 @@ export const swaggerConfig = async (app: INestApplication) => {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(SWAGGER_URL, app, documentFactory);
+  SwaggerModule.setup(SWAGGER_URL, app, documentFactory, {
+    customSiteTitle: "Api Docs",
+    customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
+  });
 };
